007531f0fbf2e52ec4e40f64e9788f2dfc8f0464
Mikołaj Michalak <majkanart117@gmail.com>
2026-04-26 22:43:03 +0200

    feat: make proper functionality

--- app/actions/workout.ts
+++ app/actions/workout.ts
@@ -2,11 +2,16 @@
 
 import {getPrisma} from "@/lib/prisma"
 import { revalidatePath } from "next/cache"
+import {auth} from "@clerk/nextjs/server"
 
 export async function createWorkout(name:string){
+    const {userId} = await auth()
+
+    if (!userId) throw new Error("Unauthorized")
     await getPrisma().workout.create({
         data:{
-            name
+            name,
+            userId
         }
     })
     revalidatePath("/");

6423531f6ec851987acd2f5a74ea1a592cf2fbb5
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-18 18:37:59 +0200

    featl: i COMLPLETED the [id]/page.tsx

---
+++ app/dashboard/workouts/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { notFound } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+import {getPrisma} from "@/lib/prisma"
+import { PageProps } from "@/app/types";
+import Link from "next/link";
+
+export default async function WorkoutsId({params}:PageProps){
+  const { id } = await params
+  const { userId } = await auth();
+  const db = getPrisma(); 
+  if(!userId) return;
+
+  const workout = await db.workout.findUnique({
+    where:{ id,userId},
+    include: {exercises: true }
+  })
+
+  if (!workout) notFound()
+  return(
+    <div>
+      {workout.exercises.length===0 ? 
+      "No exercises" :
+      workout.exercises.map((item)=>(
+      <div key={item.id}>
+      <p>{item.name}</p>
+      <p>{item.sets}</p>
+      <p>{item.reps}</p>
+      <p>{item.weight}</p>
+      </div>
+      ))
+    }
+      <button>
+        <Link href="/dashboard/workouts">
+          Come back
+        </Link>
+        </button>
+    </div>
+  )
+}

11d57493c1b2894335337c38b1dc8615bfc305cf
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-19 12:35:05 +0200

    refactor: change the create workout place

--- components/workout/WorkoutCard.tsx
+++ components/workout/WorkoutCard.tsx
@@ -4,11 +4,9 @@ import { Workout } from "@/app/types"
 export function WorkoutCard({ workout}:{workout:Workout}){
   return(
     <div>
-      <p>{workout.id}</p>
       <p>{workout.exercises.length}</p>
       <p>{workout.name}</p>
       <p>{workout.createdAt.toLocaleDateString("pl-PL")}</p>
-      <Link href={`/dashboard/workouts/${workout.id}`}>Create new workout</Link>
     </div>
   )
 }
--- components/workout/WorkoutList.tsx
+++ components/workout/WorkoutList.tsx
@@ -1,12 +1,15 @@
 import { WorkoutCard } from "./WorkoutCard"
 import { Workout } from "@/app/types"
-
+import Link from "next/link"
 export function WorkoutList({workout}:{workout:Workout []}){
   return(
     <div>
       {workout.map((item)=>(
         <WorkoutCard key={item.id} workout={item}/>
       ))}
+      <div>
+        <Link href={`/dashboard/workouts/${workout.id}`}>Create new workout</Link>
+      </div>
     </div>
   )
 }

d84fe176b92a9b6cecb1dcdfd271e1aeb241b02f
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-19 16:57:46 +0200

    feat: add a new features

--- components/workout/WorkoutList.tsx
+++ components/workout/WorkoutList.tsx
@@ -5,14 +5,18 @@ import Link from "next/link"
 export function WorkoutList({workout}:{workout:Workout []}){
   return(
     <div className="mx-auto max-w-lg text-center bg-amber-100 rounded-4xl">
-      <h1 className="text-3xl">My trainings</h1>
+      <h1 className="text-3xl">My Workouts</h1>
       {workout.length===0?
       <p>No exercises</p>:
       workout.map((item)=>(
-        <WorkoutCard key={item.id} workout={item}/>
+        <div key={item.id}>
+          <p>{item.name}</p>
+          <p>{item.createdAt.toLocaleDateString("PL-pl")}</p>
+          <WorkoutCard  workout={item}/>
+        </div>
       ))}
       <Link href="/dashboard/workouts/new" className="text-xl my-4">
-        +Add new trainings
+        +Add new workout
       </Link>
     </div>
   )

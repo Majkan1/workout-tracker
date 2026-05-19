d31b7ec083d2eb3b241cc23fcac1d98f94edb406
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-19 12:58:50 +0200

    feat: add a Link for the whole Workout Card

--- components/workout/WorkoutCard.tsx
+++ components/workout/WorkoutCard.tsx
@@ -4,9 +4,11 @@ import { Workout } from "@/app/types"
 export function WorkoutCard({ workout}:{workout:Workout}){
   return(
     <div>
+      <Link href={`/dashboard/workouts/${workout.id}`}>
       <p>{workout.exercises.length}</p>
       <p>{workout.name}</p>
       <p>{workout.createdAt.toLocaleDateString("pl-PL")}</p>
+      </Link>
     </div>
   )
 }

2574952071d1a8beb01a39ede46a5b59c14699a5
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-21 16:27:03 +0200

    feat: add stats for the user

---
+++ app/dashboard/_components/StatsGrid.tsx
@@ -0,0 +1,15 @@
+export function StatsGrid(){
+  return(
+    <div>
+      <div>
+        <p>Workouts together:</p>
+      </div>
+      <div>
+        <p>Exercises together:</p>
+      </div>
+      <div>
+        <p>Latest workout:</p>
+      </div>
+    </div>
+  )
+}

16a5ca82f46207318e2eb0fcbad695c9efbaee01
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-22 17:51:04 +0200

    refactor: update the Appshell component

--- components/shared/AppShell.tsx
+++ components/shared/AppShell.tsx
@@ -2,13 +2,23 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import Footer from '@/components/shared/Footer'
 import Navbar from '@/components/shared/Navbar'
+import { usePathname } from 'next/navigation'
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
+  const pathme = usePathname()
+  
+  const isDashboard = pathme.startsWith("/dashboard");
+  const isAuth = pathme.startsWith("/auth");
+
+  const showFooter = ! isDashboard && !isAuth 
+
   return (
     <ClerkProvider signInUrl="/auth/sign-in" signUpUrl="/auth/sign-up">
       <Navbar />
-      {children}
-      <Footer />
+      <main className='glex-grow'>
+        {children}
+      </main>
+      {showFooter && <Footer />}
     </ClerkProvider>
   )
 }

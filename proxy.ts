4eb34635bc7543ec072c29033eafe8ac43a64028
Mikołaj Michalak <majkanart117@gmail.com>
2026-05-23 02:49:43 +0200

    delete : remove the proxy.ts

--- proxy.ts
+++
@@ -1,6 +0,0 @@
-import middleware from "./middleware";
-
-export default middleware;
-
-// Re-export the config so builds referencing proxy.ts find the matcher
-export const config = (middleware as any).config;
